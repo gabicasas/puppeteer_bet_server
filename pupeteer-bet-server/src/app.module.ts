1e7c33c4f66585cd01edbb4665df0567091af008
Gabi <gabicasas@gmail.com>
2019-05-15 23:59:16 +0200

    Añadida conexion sin probar

--- pupeteer-bet-server/src/app.module.ts
+++ pupeteer-bet-server/src/app.module.ts
@@ -2,12 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BetController } from './bet/bet.controller';
+import { BetService } from './bet/bet.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'mysql',
+      database: 'testBet',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, BetController],
-  providers: [AppService],
+  providers: [AppService, BetService],
 })
 export class AppModule {}
--- pupeteer-bet-server/src/bet/bet.controller.ts
+++ pupeteer-bet-server/src/bet/bet.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { BetService } from './bet.service';
 
 @Controller('bet')
-export class BetController {}
+export class BetController {
+    constructor(private readonly betService: BetService) { }
+}
